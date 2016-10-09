2d18c1769c5e904e2afd786a3391f23431aa5742
TaimoorTariq123 <taimoor_t@yahoo.com>
2016-10-09 19:20:17 +0500

    The update of Task is done

--- src/pages/addTodo/addTodo.ts
+++ src/pages/addTodo/addTodo.ts
@@ -14,15 +14,31 @@ export class AddTodo {
     authUid: any;
     date: any;
     list: FirebaseListObservable<any[]>;
+    temp :any;
+    day: any;
+    month: any;
+    year: any;
+
+    public event = {
+        monthStart: '2016-01-01',
+        monthEnd: '2016-01-01'
+    }
 
     constructor(public navCtrl: NavController, public af: AngularFire) {
         this.date = new Date();
+        this.day = this.date.getDate();
+        this.month = this.date.getMonth();
+        this.year = this.date.getUTCFullYear();
+        this.temp = this.day.toString() +"-"+ (this.month+1).toString() +"-"+ this.year.toString();  
+        
+        console.log(this.temp);
+
+        
         this.af.auth.subscribe((auth) => { this.authUid = auth.uid });
-        this.list = af.database.list('TodoAppDatabase/users/' + this.authUid);
+        this.list = af.database.list('TodoAppDatabase/users/' + this.authUid + '/' + this.temp);
     }
 
     create() {
-        this.af.auth.subscribe((auth) => { this.authUid = auth.uid });
         this.list.push({ mytodo: this.todoText }).then((data) => {
             this.navCtrl.pop();
         });
--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 import {AddTodo} from '../addTodo/addTodo';
 import {FirebaseListObservable, AngularFire} from 'angularfire2';
 
@@ -13,17 +13,82 @@ import {FirebaseListObservable, AngularFire} from 'angularfire2';
 export class Dashboard {
 
     list: FirebaseListObservable<any[]>;
+    items: FirebaseListObservable<any[]>;
     authUid: any;
-    constructor(public navCtrl: NavController, public af:AngularFire ) {
+    testCheckboxOpen: any;
+    testCheckboxResult: any;
+    temp: string[] = [];
+    flag: boolean;
+    completeData :any;
+    day: any;
+    month: any;
+    year: any;
+    date: any;
+
+    constructor(public navCtrl: NavController, public af: AngularFire, public alertCtrl: AlertController) {
         this.af.auth.subscribe((auth) => { this.authUid = auth.uid });
-        this.list = this.af.database.list('TodoAppDatabase/users/' +this.authUid);
+        this.list = this.af.database.list('TodoAppDatabase/users/' + this.authUid );
+        this.load();
+
+    }
+
+    load() {
+
+        this.date = new Date();
+        this.day = this.date.getDate();
+        this.month = this.date.getMonth();
+        this.year = this.date.getUTCFullYear();
+        this.completeData = this.day.toString() +"-"+ (this.month+1).toString() +"-"+ this.year.toString();  
+
+
+         this.items = this.af.database.list('TodoAppDatabase/users/' + this.authUid+ '/' +this.completeData, { preserveSnapshot: true });
+            this.items
+            .subscribe(snapshots => {
+                snapshots.forEach(snapshot => {
+                    for(var i = 0; i < this.temp.length; ++i ) {
+                        if(this.temp[i] == snapshot.val().mytodo) {
+                            this.flag = true;
+                        }
+                    }
+                    if(this.flag == true) {
+                        this.flag = false;
+                    }
+                    else {
+                        this.temp.push(snapshot.val().mytodo);
+                    }
+                });
+            })
     }
 
     addNewTodo() {
+        console.log("newData", this.list[1]);
         this.navCtrl.push(AddTodo);
-        console.log(this.list);
+
     }
 
+    showCheckbox() {
+        let alert = this.alertCtrl.create();
+        alert.setTitle('Which planets have you visited?');
 
+        for (var i = 0; i < this.temp.length; ++i) {
+            alert.addInput({
+                type: 'checkbox',
+                label: this.temp[i],
+                value: i.toString(),
+                checked: false
+            });
+        }
+
+        alert.addButton('Cancel');
+        alert.addButton({
+            text: 'Okay',
+            handler: data => {
+                console.log('Checkbox data:', data);
+                this.testCheckboxOpen = false;
+                this.testCheckboxResult = data;
+            }
+        });
+        alert.present();
+    }
 
 }
